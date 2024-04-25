4c1fc23dcb01b20e81f99549de219ea8f14dc57a
Modesty Zhang <modesty.zhang@servicenow.com>
2024-04-25 14:27:32 -0700

    remote: Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 function main() {
     console.log('this could also be an entry point');
-    console.log('add one more log to the entrance')
+    console.log('add one more log to the entrance');
+    console.info('Jared is watching');
 }
